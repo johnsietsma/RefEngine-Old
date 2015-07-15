5243b43bc9152e9cc4c870d62ea1a5ad49c64efe
John Sietsma <johns@aie.edu.au>
2015-07-15 17:44:26 +1000

    Added mouse movement to the fly camera;

--- RefEngine/src/FlyInput.cpp
+++ RefEngine/src/FlyInput.cpp
@@ -1,13 +1,14 @@
 #include "FlyInput.h"
 
 #include "GLFW\glfw3.h"
+#include "glm\mat4x4.hpp"
 
 using namespace reng;
 
 void FlyInput::Update(GLFWwindow* pWindow, double deltaTime)
 {
 	HandleKeyboard(pWindow, deltaTime);
-	HandleMouse(pWindow);
+	HandleMouse(pWindow, deltaTime);
 }
 
 
@@ -54,7 +55,72 @@ void FlyInput::HandleKeyboard(GLFWwindow* pWindow, double deltaTime)
 	}
 }
 
-void FlyInput::HandleMouse(GLFWwindow* pWindow)
+void FlyInput::HandleMouse(GLFWwindow* pWindow, double deltaTime)
 {
+	if (glfwGetMouseButton(pWindow, GLFW_MOUSE_BUTTON_2) == GLFW_PRESS)
+	{
+		int width, height;
+		glfwGetFramebufferSize(pWindow, &width, &height);
 
+		if (m_rotateButtonClicked == false)
+		{
+			m_cursorX = (float)floor(width / 2.0);
+			m_cursorY = (float)floor(height / 2.0);
+
+			m_rotateButtonClicked = true;
+		}
+		else
+		{
+			double mouseX, mouseY;
+			glfwGetCursorPos(pWindow, &mouseX, &mouseY);
+
+			double xOffset = mouseX - m_cursorX;
+			double yOffset = mouseY - m_cursorY;
+
+			CalculateRotation(deltaTime, xOffset, yOffset);
+
+		}
+
+		glfwSetCursorPos(pWindow, width / 2, height / 2);
+	}
+	else
+	{
+		m_rotateButtonClicked = false;
+	}
+}
+
+
+void FlyInput::CalculateRotation(double deltaTime, double xOffset, double yOffset)
+{
+	Transform transform = GetTransform();
+	glm::mat4x4 transformMatrix = transform.GetMartix();
+
+	if (xOffset != 0.0)
+	{
+		glm::mat4 rot = glm::rotate((float)(m_rotationSpeed * deltaTime * -xOffset), glm::vec3(0, 1, 0));
+		transformMatrix *= rot;
+	}
+
+	if (yOffset != 0.0)
+	{
+		glm::mat4 rot = glm::rotate((float)(m_rotationSpeed * deltaTime * -yOffset), glm::vec3(1, 0, 0));
+		transformMatrix *= rot;
+	}
+
+	//Clean up rotation
+	glm::mat4 oldTrans = transformMatrix;
+	glm::mat4 trans;
+	glm::vec3 worldUp = glm::vec3(0, 1, 0);
+
+	//Right
+	trans[0] = glm::normalize(glm::vec4(glm::cross(worldUp, oldTrans[2].xyz()), 0));
+	//Up
+	trans[1] = glm::normalize(glm::vec4(glm::cross(oldTrans[2].xyz(), trans[0].xyz()), 0));
+	//Forward
+	trans[2] = glm::normalize(oldTrans[2]);
+
+	//Position
+	trans[3] = oldTrans[3];
+
+	SetTransform(trans);
 }
--- RefEngine/src/FlyInput.h
+++ RefEngine/src/FlyInput.h
@@ -9,20 +9,25 @@ namespace reng {
 class FlyInput {
 public:
 	FlyInput( Camera* pCamera ) :
-		m_pCamera(pCamera)
+		m_pCamera(pCamera),
+		m_rotateButtonClicked(false)
 	{}
 
 	void Update(GLFWwindow* pWindow, double deltaTime);
 
 private:
 	void HandleKeyboard(GLFWwindow* pWindow, double deltaTime);
-	void HandleMouse(GLFWwindow* pWindow);
+	void HandleMouse(GLFWwindow* pWindow, double deltaTime);
 
 	Transform GetTransform()  { return m_pCamera->GetTransform(); }
 	void SetTransform(const Transform& transform) { m_pCamera->SetTransform(transform); }
+	void CalculateRotation(double deltaTime, double xOffset, double yOffset);
 
 	Camera* m_pCamera;
 	float m_flySpeed = 1;
+	float m_rotationSpeed = 1;
+	bool m_rotateButtonClicked;
+	float m_cursorX, m_cursorY;
 };
 
 }
